b25a8a850bd2439d81bd2bdcdfcd2d739ce0721a
Tini79 <artinichan79@gmail.com>
2024-05-06 22:49:01 +0800

    chnaged url for testing on production

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,14 +2,15 @@
 import { getSlugs } from "./lib/data";
 
 export default async function sitemap() {
-  const baseUrl = "https://infinityprittyjewellery.com"
+  // const baseUrl = "https://infinityprittyjewellery.com"
+  const baseUrl = "https://vercel.com/tinis-projects-9f5a40a4/infinity-app-client"
   const response = await getSlugs()
   console.log(response, 'sssss');
   
   const categorySlugs = response?.map((data: any) => {
     return {
       url: `${baseUrl}/category/${data?.slug}`,
-      // lastModified: new Date(data?.created_at)
+      lastModified: new Date(data?.created_at)
     }
   })
 

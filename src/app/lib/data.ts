acea4dab903836c05fcfbd09e783ba8706660436
Tini79 <artinichan79@gmail.com>
2024-06-08 08:19:23 +0800

    deleted console

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -81,7 +81,6 @@ export async function getTestimonials() {
     const { data } = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/testimonials`, options)
     return data.data
   } catch (error: any) {
-    console.log(error);
     if (error.response.data.statusCode == 401) {
       revalidatePath("/logout")
       redirect("/logout")
